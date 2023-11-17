d9d4449991e3ef3f1f6312e1307ba753e3c83fbe
FelipeOvidio <f.costaovidio@gmail.com>
2023-11-17 17:00:08 -0300

    Importação das rotas do usuarios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
+const user = require('./router/usersRote');
 const app = express();
 
 app.use(express.json())
+app.use(user)
 
 app.listen(process.env.PORT, () => {
     return console.log('Start Serv Port 3000');
