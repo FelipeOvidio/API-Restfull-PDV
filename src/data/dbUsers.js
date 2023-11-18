6010b71e75a329956bf2fa6200bc782d204c2092
FelipeOvidio <f.costaovidio@gmail.com>
2023-11-18 14:30:20 -0300

    Query para inclui registros no bando de dados

--- src/data/dbUsers.js
+++ src/data/dbUsers.js
@@ -4,6 +4,20 @@ module.exports = {
     async getUsersAll() {
         const listUser = await knex.select('id', 'nome', 'email').from('usuarios')
         return listUser
+    },
+
+    async getUserEmail(email) {
+        const existEmail = await knex('usuarios').where({ email }).first()
+        if (existEmail) {
+            return true
+        }
+
+        return existEmail
+
+    },
+
+    async insertUser(user) {
+        return await knex('usuarios').insert(user)
     }
 
 }
