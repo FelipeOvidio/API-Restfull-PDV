c157aa7609418323a27a68c4dc5c050a31daa5d6
FelipeOvidio <f.costaovidio@gmail.com>
2023-11-18 14:29:11 -0300

    Validação dos dados do corpo da requisição

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,31 @@
-const { getUsersAll } = require("../data/dbUsers")
+const bcrypt = require('bcrypt')
+const { getUsersAll, getUserEmail, insertUser } = require("../data/dbUsers")
+const registerUser = async (req, res) => {
+    const { nome, email, senha } = req.body
+    try {
+        const existEmail = await getUserEmail(email)
+        if (existEmail) {
+            return res.status(409).json({ mensage: 'Email ja cadastrado' })
+        }
+        const newUser = {
+            nome,
+            email,
+            senha: await bcrypt.hash(senha, 10)
+        }
+        await insertUser(newUser)
+
+        const { senha: _, ...dataUser } = newUser
+
+        return res.status(200).json(dataUser)
+
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({ mensagem: 'Error interno no servidor' })
+    }
+}
+
 
 
 const listUsersAll = async (req, res) => {
@@ -11,5 +38,6 @@ const listUsersAll = async (req, res) => {
 }
 
 module.exports = {
+    registerUser,
     listUsersAll
 }
