f2e536c1c848c7c75dc837998cd9a4a4adcee024
FelipeOvidio <f.costaovidio@gmail.com>
2023-11-18 14:31:35 -0300

    Criação de rota de cadastração de usuario com middlware de validação do corpo da requisição

--- src/router/usersRouter.js
+++ src/router/usersRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { listUsersAll } = require('../controller/userController');
+const { registerUser, listUsersAll } = require('../controller/userController');
+const schema = require('../schema/schemaUser');
+const { validate } = require('../middleware/validation');
+
+
 
 const user = express();
-
+user.post('/usuario', validate(schema.verifyUser), registerUser)
 user.get('/usuarios', listUsersAll)
 
 
